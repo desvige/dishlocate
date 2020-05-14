30d7d1dbd2244d3726de48992f32db04ff6f61d2
Olivier Sallenave <ol.sall@gmail.com>
2020-05-14 17:42:20 +0200

    Update get-menu-url-test.clj

--- test/geolocation/gmaps/get-menu-url-test.clj
+++ test/geolocation/gmaps/get-menu-url-test.clj
@@ -8,13 +8,3 @@
   (with-redefs [get-place-details-request mock-get-place-details-request]
     (is (= (get-gmaps-page-url "foobar1337")
       "https://maps.google.com/?cid=5232738768037132369"))))
-
-; (defn mock-get-gmaps-page-url [place-id]
-;   "test/geolocation/gmaps/gmaps-page.html")
-
-; (deftest test-get-menu-url
-;   (with-redefs [get-gmaps-page-url mock-get-gmaps-page-url]
-;     (is (= (get-menu-url "foobar1337")
-;       "https://www.mcdonalds.fr/produits/menus"))))
-
-; TODO: Test when gmaps-page-url is nil
